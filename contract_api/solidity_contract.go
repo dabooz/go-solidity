b821b541afe1d9fc796fd8ed2daae8d1f4126a68
Dave Booz <booz@us.ibm.com>
2015-11-17 10:38:51 -0500

    go back to old return 0 behavior

--- contract_api/solidity_contract.go
+++ contract_api/solidity_contract.go
@@ -183,7 +183,8 @@ func (self *SolidityContract) Invoke_method(method_name string, params []interfa
 						if rpcResp.Result != "0x" {
 							result, err = self.decodeOutputString(method_name, rpcResp.Result.(string)[2:])
 						} else {
-							err = &RPCError{fmt.Sprintf("RPC invocation returned %v, the EVM probably failed executing method %v.", rpcResp.Result, method_name)}
+							result = 0
+							// err = &RPCError{fmt.Sprintf("RPC invocation returned %v, the EVM probably failed executing method %v.", rpcResp.Result, method_name)}
 						}
 					}
 				}
