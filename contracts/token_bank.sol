7ce2e025fbac1959e129f0254f24bdb03694e189
Dave Booz <booz@us.ibm.com>
2015-11-19 07:46:51 -0500

    remove debug code from bank

--- contracts/token_bank.sol
+++ contracts/token_bank.sol
@@ -66,16 +66,10 @@ contract token_bank {
     event ProposerVerified(uint indexed _event_code, address _from, address _to, address indexed _contract);
     event ProposalCompleted(uint indexed _event_code, address _from, address _to, address indexed _contract);
 
-    uint escrow_debug;
-    
-    function get_debug() constant returns (uint r) {
-        return escrow_debug;
-    }
     // Constructor, runs when this contract is deployed (aka instantiated)
     function token_bank() {
         minter = msg.sender;
         total_currency = 1000000000000000;
-        escrow_debug = 0;
     }
 
     // Send currency to another party
@@ -140,26 +134,19 @@ contract token_bank {
     // a) there isn't already a proposal between the parties involving the input smart contract
     // b) the proposal includes a non-zero amount of crytocurrency
     function create_escrow(address _cp, address _contract, uint _amount) returns (bool rv) {
-        escrow_debug = 0;
         if (_amount > 0 && hasAmount(tx.origin,_amount)) {
-            escrow_debug = 1;
             var prop = escrow[tx.origin].counter_parties[_cp].proposals[_contract];
-            escrow_debug = 2;
             if (prop.amount == 0) {
-                escrow_debug = 3;
                 escrow[tx.origin].counter_parties[_cp].proposals[_contract] = proposal({proposer_vote:false,
                                                                                         counter_party_vote:false,
                                                                                         amount:_amount});
                 balances[tx.origin] -= _amount;
                 NewProposal(uint(event_codes.escrow_created), tx.origin, _cp, _contract);
-                escrow_debug = 4;
                 return true;
             } else {
-                escrow_debug = 5;
                 return false;
             }
         } else {
-            escrow_debug = 6;
             return false;
         }
     }
