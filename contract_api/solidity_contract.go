ed95eb6c3ebfe6ec1560bce659d65c1d9d5df35f
Dave Booz <booz@us.ibm.com>
2016-04-22 14:05:14 -0400

    fix up log stats

--- contract_api/solidity_contract.go
+++ contract_api/solidity_contract.go
@@ -446,7 +446,18 @@ func (self *SolidityContract) log_stats(rpcResp *rpcGetTransactionResponse) {
 	// If logging blockchain stats, dump them to the log
 	if self.logBlockchainStats != "" {
 		block_num := rpcResp.Result.BlockNumber
-		_,_ = self.Call_rpc_api("eth_getBlockByNumber", &MultiValueParams{block_num, false})
+		if out, err := self.Call_rpc_api("eth_getBlockByNumber", &MultiValueParams{block_num, false}); err != nil {
+			self.logger.Debug("Error", err.Error())
+			return
+		} else {
+			var rpcResp *rpcGetBlockByNumberResponse = new (rpcGetBlockByNumberResponse)
+			if err := json.Unmarshal([]byte(out), rpcResp); err == nil {
+				if rpcResp.Error.Message == "" {
+					gasUsed, _ := strconv.ParseUint(rpcResp.Result.GasUsed[2:], 16, 64)
+					fmt.Printf("Gas used: %v\n", gasUsed)
+				}
+			}
+		}
 	}
 }
 
@@ -1496,6 +1507,38 @@ type rpcResponse struct {
 		Code    int    `json:"code"`
 		Message string `json:"message"`
 	} `json:"error"`
+}
+
+type rpcGetBlockByNumberResponse struct {
+	Id      string     `json:"id"`
+	Version string     `json:"jsonrpc"`
+	Result  rpcBlock   `json:"result"`
+	Error   struct {
+		Code    int    `json:"code"`
+		Message string `json:"message"`
+	} `json:"error"`
+}
+
+type rpcBlock struct {
+	Number           string   `json:"number"`
+	Hash             string   `json:"hash"`
+	ParentHash       string   `json:"parentHash"`
+	Nonce            string   `json:"nonce"`
+	Sha3Uncles       string   `json:"sha3Uncles"`
+	LogsBloom        string   `json:"logsBloom"`
+	TransactionsRoot string   `json:"transactionsRoot"`
+	StateRoot        string   `json:"stateRoot"`
+	ReceiptRoot      string   `json:"receiptRoot"`
+	Miner            string   `json:"miner"`
+	Difficulty       string   `json:"difficulty"`
+	TotalDifficulty  string   `json:"totalDifficulty"`
+	ExtraData        string   `json:"extraData"`
+	Size             string   `json:"size"`
+	GasLimit         string   `json:"gasLimit"`
+	GasUsed          string   `json:"gasUsed"`
+	Timestamp        string   `json:"timestamp"`
+	Transactions   []string   `json:"transactions"`
+	Uncles         []string   `json:"uncles"`
 }
 
 type rpcGetTransactionResponse struct {
